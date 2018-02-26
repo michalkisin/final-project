0f997d835344547a8ddbbc6b67676d49434bc85e
michalkisin <30836734+michalkisin@users.noreply.github.com>
2018-02-26 23:06:23 +0200

    update files

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+$(function(){
+  $("#header").load("header.html"); 
+  $("#footer").load("footer.html"); 
+});
+
 jQuery(document).ready(function($) {
   $(".mobile-menu-list").hide();
   $(".mobile-menu").click(function() {
       $(".mobile-menu-list").slideToggle('slow');
+      $(".topics-search-header").show();
   });
 });
 
@@ -16,27 +22,46 @@ jQuery(document).ready(function($) {
   $(".comment-red").hide();
   $(".seriously img:nth-of-type(2)").hide();
   $(".seriously-red").click(function() {
-      $(".article:nth-of-type(n+1)").css( "background-color", "#f6f4f7");
-      $(".article").css( "color", "#bc3428");
-      $(".article a").css( "color", "#bc3428");
-      $(".comment-white").hide();
-      $(".comment-red").show();
-      $(".author-img img").css("border", "5px solid #bc3428");
-      $(".seriously-red").text("Change it back");
-      $(".article:nth-of-type(n+2)").css( "background-color", "#fcfafd");
-      $(".article:nth-of-type(n+3)").css( "background-color", "#f2f0f3");
-      $(".article:nth-of-type(n+4)").css( "background-color", "#f6f4f7");
-      $(".seriously-red").css("color", "gray");
-      $(".seriously-red").css("border", "2px solid gray");
-      $(".seriously-red").css("background", "#fff");
-      $(".main-content-header a").css("background", "#e82429");
-      $(".main-content-header a").css("color", "#fff");
-      $(".seriously img:nth-of-type(1)").hide();
-      $(".seriously img:nth-of-type(2)").show();
-
+	  if($(".seriously-red").text() == 'Change it back'){
+		  $(".article:nth-of-type(n+1)").css( "background-color", "#bc3428");
+	      $(".article").css( "color", "#ffffff");
+	      $(".article a").css( "color", "#ffffff");
+	      $(".comment-white").show();
+	      $(".comment-red").hide();
+	      $(".author-img img").css("border", "5px solid #ffffff");
+	      $(".article:nth-of-type(n+2)").css( "background-color", "#c7372a");
+	      $(".article:nth-of-type(n+3)").css( "background-color", "#b13125");
+	      $(".article:nth-of-type(n+4)").css( "background-color", "#bc3428");
+	      $(".seriously-red").css("color", "#fff");
+	      $(".seriously-red").css("border", "1px solid #0071c1");
+	      $(".seriously-red").css("background", "linear-gradient(to bottom, #0071c1, #b6d6ed)");
+	      $(".main-content-header a").css("background", "#fff");
+	      $(".main-content-header a").css("color", "#282634");
+	      $(".seriously img:nth-of-type(1)").show();
+	      $(".seriously img:nth-of-type(2)").hide();
+	      $(".seriously-red").text("Change my color");
+	  }
+	  else{
+	      $(".article:nth-of-type(n+1)").css( "background-color", "#f6f4f7");
+	      $(".article").css( "color", "#bc3428");
+	      $(".article a").css( "color", "#bc3428");
+	      $(".comment-white").hide();
+	      $(".comment-red").show();
+	      $(".author-img img").css("border", "5px solid #bc3428");
+	      $(".article:nth-of-type(n+2)").css( "background-color", "#fcfafd");
+	      $(".article:nth-of-type(n+3)").css( "background-color", "#f2f0f3");
+	      $(".article:nth-of-type(n+4)").css( "background-color", "#f6f4f7");
+	      $(".seriously-red").css("color", "gray");
+	      $(".seriously-red").css("border", "2px solid gray");
+	      $(".seriously-red").css("background", "#fff");
+	      $(".main-content-header a").css("background", "#e82429");
+	      $(".main-content-header a").css("color", "#fff");
+	      $(".seriously img:nth-of-type(1)").hide();
+	      $(".seriously img:nth-of-type(2)").show();
+	      $(".seriously-red").text("Change it back");
+	  }
   });
 });
-
 
 
 jQuery(document).ready(function($) {
@@ -89,30 +114,174 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 };
+//LIGHTBOX
+function openModal(src){
+	var modal = document.getElementById('myModal');
+	var modalImg = document.getElementById("img01");
+	modal.style.display = "block";
+    modalImg.src = src;
+    var span = document.getElementsByClassName("close")[0];
+}
+function closeModal(){
+	document.getElementById('myModal').style.display = "none";
+}
 
-//TABS
-$(document).ready(function(){
+//ADD COMMENTS
+function appendCmnt(){
+  if(document.getElementById('cmntName').value == ''){
+    document.getElementById('validVal').innerHTML = "Name can not be empty";
+    document.getElementById('validVal').style.visibility = 'visible';
+    return;
+  }
+  if(document.getElementById('cmntText').value == ''){
+    document.getElementById('validVal').innerHTML = "Comment can not be empty";
+    document.getElementById('validVal').style.display = '';
+    document.getElementById('validVal').style.visibility = 'visible';
+    return;
+  }
+  var dvchild = document.createElement("DIV");
+  dvchild.className = "all-comments-row";
+  dvchild.innerHTML = "<span class=\"all-comments-img\"><img src=\"images/items/default-avatar.png\"></span>";
+  dvchild.innerHTML += "<span class=\"all-comments-name\">";
+  dvchild.innerHTML += document.getElementById('cmntName').value;
+  dvchild.innerHTML += "</span><div class=\"all-comments-message\">";
+  dvchild.innerHTML += document.getElementById('cmntText').value;
+  dvchild.innerHTML += "</div>";
+  
+  var firstCmnt = document.getElementById('all-comments').getElementsByTagName("div")[0];
+  document.getElementById('all-comments').insertBefore(dvchild, firstCmnt);
+  
+  document.getElementById('cmntName').value = '';
+  document.getElementById('cmntText').value = '';
+  document.getElementById('validVal').style.visibility = 'hidden';
+}
+
+//BOOKS
+//tabs
+document.getElementById('tab-2').style.display = 'none';
+var lnks = document.getElementsByClassName('tab-link');
+//lnks[0].className = 'active';
+function openPage(elm) {
 	
-	$('ul.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
-
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
-
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	})
-
-});
-
+	var lnks = document.getElementsByClassName('tab-link');
+	if(elm == 'tab2'){
+		document.getElementById('tab-1').style.display = 'none';
+		document.getElementById('tab-2').style.display = 'block';
+		
+		lnks[0].style.backgroundColor = '#811d15';
+		lnks[1].style.backgroundColor = '#bc3428';
+	}
+	else{
+		document.getElementById('tab-1').style.display = 'block';
+		document.getElementById('tab-2').style.display = 'none';
+		lnks[0].style.backgroundColor = '#bc3428';
+		lnks[1].style.backgroundColor = '#811d15';
+	}
+}
+//BOOKS
+//load more
+function loadmore(btn){
+  if(document.getElementById('more-books').style.height == document.getElementById('more-books').scrollHeight + 'px'){
+    document.getElementById('more-books').style.height = '0px';
+    btn.innerText = 'LOAD MORE';
+  }else{
+    document.getElementById('more-books').style.height = document.getElementById('more-books').scrollHeight + 'px';
+    btn.innerText = 'LOAD LESS';
+  }
+  document.getElementById('more-books').style.height = document.getElementById('more-books').scrollHeight + 'px';
+  btn.style.display = 'none';
+}
 
 //EVENTS
+//load more
+function loadmoreEv(btn){
+  if(document.getElementById('more-events').style.height == document.getElementById('more-events').scrollHeight + 'px'){
+    document.getElementById('more-events').style.height = '0px';
+    btn.innerText = 'LOAD MORE';
+  }else{
+    document.getElementById('more-events').style.height = document.getElementById('more-events').scrollHeight + 'px';
+    btn.innerText = 'LOAD LESS';
+  }
+  document.getElementById('more-events').style.height = document.getElementById('more-events').scrollHeight + 'px';
+  btn.style.display = 'none';
+}
 
-jQuery(document).ready(function($) {
-  $(".events-item-front").click(function() {
-      $(".events-item-back").show();
-  });
+//ITEM
+//scroll to all comments
+  $(document).ready(function(){
+	$('.comment').click(function(){
+		$('html, body').animate({scrollTop : 2650},800);
+		return false;
+	});
+	
 });
+
+//ARTICLES
+//load more
+function loadmoreArc(btn){
+  if(document.getElementById('more-articles').style.height == document.getElementById('more-articles').scrollHeight + 'px'){
+    document.getElementById('more-articles').style.height = '0px';
+    btn.innerText = 'LOAD MORE';
+  }else{
+    document.getElementById('more-articles').style.height = document.getElementById('more-articles').scrollHeight + 'px';
+    btn.innerText = 'LOAD LESS';
+  }
+  document.getElementById('more-articles').style.height = document.getElementById('more-articles').scrollHeight + 'px';
+  btn.style.display = 'none';
+}
+
+//PLAYGROUND
+//handicrafts
+//load more
+function loadmoreHend(btn){
+  if(document.getElementById('more-handicrafts').style.height == document.getElementById('more-handicrafts').scrollHeight + 'px'){
+    document.getElementById('more-handicrafts').style.height = '0px';
+    btn.innerText = 'LOAD MORE';
+  }else{
+    document.getElementById('more-handicrafts').style.height = document.getElementById('more-handicrafts').scrollHeight + 'px';
+    btn.innerText = 'LOAD LESS';
+  }
+  document.getElementById('more-handicrafts').style.height = document.getElementById('more-handicrafts').scrollHeight + 'px';
+  btn.style.display = 'none';
+}
+//recipes
+//load more
+function loadmoreRes(btn){
+  if(document.getElementById('more-recipes').style.height == document.getElementById('more-recipes').scrollHeight + 'px'){
+    document.getElementById('more-recipes').style.height = '0px';
+    btn.innerText = 'LOAD MORE';
+  }else{
+    document.getElementById('more-recipes').style.height = document.getElementById('more-recipes').scrollHeight + 'px';
+    btn.innerText = 'LOAD LESS';
+  }
+  document.getElementById('more-recipes').style.height = document.getElementById('more-recipes').scrollHeight + 'px';
+  btn.style.display = 'none';
+}
+//art
+//load more
+function loadmoreArt(btn){
+  if(document.getElementById('more-art').style.height == document.getElementById('more-art').scrollHeight + 'px'){
+    document.getElementById('more-art').style.height = '0px';
+    btn.innerText = 'LOAD MORE';
+  }else{
+    document.getElementById('more-art').style.height = document.getElementById('more-art').scrollHeight + 'px';
+    btn.innerText = 'LOAD LESS';
+  }
+  document.getElementById('more-art').style.height = document.getElementById('more-art').scrollHeight + 'px';
+  btn.style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
